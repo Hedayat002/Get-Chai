c9af62877d63d6440d8597d38fccc5da3ef58f30
Hedayat Ali <132713488+Hedayat002@users.noreply.github.com>
2026-05-09 13:45:10 +0530

    Refactor connectDb to use MONGO_URI

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,62 +1,28 @@
 import mongoose from "mongoose";
 
+const cached = global.mongoose || (global.mongoose = { conn: null, promise: null });
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const connectDb = async () => {
+  const MONGO_URI = process.env.MONGO_URI;
+  if (!MONGO_URI) {
+    throw new Error("MONGO_URI environment variable is not defined.");
+  }
 
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI");
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDb() {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
+    cached.promise = mongoose.connect(MONGO_URI, {
+      bufferCommands: false,
+    }).then((mongooseInstance) => {
+      console.log(`MongoDB Connected: ${mongooseInstance.connection.host}`);
+      return mongooseInstance;
     });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-}
+};
 
 export default connectDb;
-
-
-
-// import mongoose from "mongoose";
-
-// const cached = global.mongoose || (global.mongoose = { conn: null, promise: null });
-
-// const connectDb = async () => {
-//   const MONGO_URI = process.env.MONGO_URI;
-//   if (!MONGO_URI) {
-//     throw new Error("MONGO_URI environment variable is not defined.");
-//   }
-
-//   if (cached.conn) {
-//     return cached.conn;
-//   }
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGO_URI, {
-//       bufferCommands: false,
-//     }).then((mongooseInstance) => {
-//       console.log(`MongoDB Connected: ${mongooseInstance.connection.host}`);
-//       return mongooseInstance;
-//     });
-//   }
-
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// };
-
-// export default connectDb;
