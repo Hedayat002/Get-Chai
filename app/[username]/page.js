0b0fc9f229da857e1d4aac24bce41e5cd1e7c0bf
Hedayat Ali <132713488+Hedayat002@users.noreply.github.com>
2026-05-22 21:13:46 +0530

    change

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -4,22 +4,34 @@ import { notFound } from 'next/navigation';
 import connectDb from '@/db/connectDb';
 import User from '@/models/User';
 
-
 const Username = async ({ params }) => {
-  //if the username is not persent in the database , show a 404 page 
-  const checkUser = async ()=>{
+  // 1. Params ko sabse pehle await kijiye
+  const { username } = await params;
+
+  // 2. Database check function
+  const checkUser = async () => {
     await connectDb()
-    let u = await User.findOne({ username: params.username })
-    if (!u) return notFound()
-
+    
+    // Case-insensitive check lagayein taaki lowercase/uppercase ka lafda na ho
+    let u = await User.findOne({ 
+      username: { $regex: new RegExp(`^${username}$`, "i") } 
+    })
+    
+    if (!u) {
+      return notFound() // Agar user nahi mila toh Next.js 404 page dikha dega
+    }
+    return u;
   }
-    await checkUser()
 
+  // Check user ko execute karein
+  await checkUser()
 
   return (
     <>
-      <PaymentPage username={params.username} />
+      {/* 3. Awaited username ko hi prop me pass karein */}
+      <PaymentPage username={username} />
     </>
   )
 }
+
 export default Username
